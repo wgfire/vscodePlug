2794223ce4bfde44842e2cccb9a541ca3bb7b81e
王港 <wangg11@mingyuanyu.com>
2021-08-19 17:54:38 +0800

    增加清空console插件

---
+++ module/clearCD.js
@@ -0,0 +1,34 @@
+/**
+ * 清空console 和 debugger
+ */
+const strUtils = require("../utils/string");
+const vscode = require("vscode");
+
+const clearCd = (editor) => {
+  // 获取用户文本内容
+  console.log(editor.document, "文本内容");
+  let Text = editor.document.getText();
+  const reg = /(console\.log(.*);?)|(debugger\S*)/g;
+  let writeText = Text.replace(reg, "");
+  console.log(writeText, "清空的");
+  // editor.document.set;
+  vscode.workspace.fs.writeFile(vscode.Uri.file(editor.document.fileName), strUtils.stringToUint8Array(writeText));
+};
+
+module.exports = clearCd;
+
+// const text = `
+// export const GET_WEBSITE_CONFIG = 'app/';
+// export function getWebsiteConfig() {}
+
+// console.log('xxx');
+// debugger
+// `;
+// console.log(text.match(/^.*[console.log('\S')]/));
+
+// console.log(
+//   text.replace(/(console\.log(.*))|(debugger)/g, (str) => {
+//     console.log(str, "匹配到");
+//     return "";
+//   })
+// );
--- module/review/decoration.js
+++ module/review/decoration.js
@@ -10,46 +10,58 @@ const DecorationType = window.createTextEditorDecorationType({
   backgroundColor: "#52ACFF",
   color: "white",
 });
-
+/**
+ * 每一个主题的所对应的颜色
+ */
 const DecorationTypes = {
   Perf: window.createTextEditorDecorationType({
     border: "1px",
-    borderStyle: "dotted", //"solid",
+    borderStyle: "dotted",
     borderColor: "#08AEEA",
     backgroundColor: "#08AEEA",
     color: "white",
   }),
   Bug: window.createTextEditorDecorationType({
     border: "1px",
-    borderStyle: "dotted", //"solid",
+    borderStyle: "dotted",
     borderColor: "#F76B1C",
     backgroundColor: "#F76B1C",
     color: "white",
   }),
   Format: window.createTextEditorDecorationType({
     border: "1px",
-    borderStyle: "dotted", //"solid",
+    borderStyle: "dotted",
     borderColor: "#FBAB7E",
-    backgroundColor: "#FBAB7E",
-    color: "white",
+    backgroundColor: "#ffcc00",
+    color: "#1f1f1f",
   }),
 };
 
 class Decoration {
   constructor() {
     this.editor = window.activeTextEditor;
-    this.regS = [/\.[Perf|Bug|Format]+/g, /(?<=\*@reviewContent(\s\S)?)([\S\s]*?)(?=\*\/)/g]; ///\w*-- Perf/g;
+
+    this.regS = [/\.[Perf|Bug|Format]+/g, /(?<=\*@reviewContent(\s\S)?)([\S\s]*?)(?=\*\/)/g];
+
     this.timeout = null;
+
     this.reviewType = []; // 为review类型添加颜色
+
     this.reviewContent = []; // @ReviewContent内容添加颜色
+
+    this.review = {};
+
     window.onDidChangeActiveTextEditor(() => {
-      console.log(this.editor.document, "文本内容1");
+      console.log(this.editor.document, "切换文件");
+
       this.editor = window.activeTextEditor;
+
       this.triggerUpdateDecorations();
     });
 
     workspace.onDidChangeTextDocument(() => {
-      console.log(this.editor.document, "文本内容2");
+      console.log(this.editor.document, "改变当前文件内容");
+
       this.triggerUpdateDecorations();
       //this.DecNumber();
     });
@@ -66,18 +78,21 @@ class Decoration {
   }
   Decoration() {
     this.init();
+    this.findEditeContent();
     this.setDecoration();
   }
   init() {
-    // 初始化找到的内容
-    this.reviewType = this.reviewType.splice();
-    this.reviewContent = this.reviewContent.splice();
+    // 初始化数据。
+    this.reviewType = [];
+    this.reviewContent = [];
   }
-  // 获取正则指定的内容
+  // 根据正则指定的内容
   findEditeContent() {
     // 获取当前文档的全部信息
     let doc = this.editor.document;
+
     let text = doc.getText();
+
     let match;
 
     this.regS.forEach((reg, index) => {
@@ -86,31 +101,63 @@ class Decoration {
       while ((match = reg.exec(text))) {
         // 获取数字开始和结束的位置
         const startPos = doc.positionAt(match.index + 1);
+
         const endPos = doc.positionAt(match.index + match[0].length);
-        const line = new vscode.Position(match.index, match.index + match[0].length);
-        console.log("开始寻找", doc.getText(new Range(startPos, endPos)));
-        // 当匹配第一个的时候 确定当前的主题色
-        if (index === 0) type = doc.getText(new Range(startPos, endPos));
+
+        const line = new Range(startPos, endPos);
+
+        if (index === 0) type = doc.getText(line);
+
+        console.log(type, "主题颜色");
+
         const decoration = {
-          range: new Range(startPos, endPos),
+          range: line,
         };
-        // this.review = {
-        //   type: DecorationTypes[type] ? DecorationTypes[type] : DecorationTypes["Perf"],
-        //   rangs: [],
-        // };
-        this.reviewType.push(decoration);
+
+        const review = {
+          type: type,
+          decoration,
+        };
+
+        this.reviewType.push(review);
       }
 
       console.log("截取到的字符", this.reviewType);
     });
   }
 
+  /**
+   * 拿到rang数据设置背景颜色
+   */
   setDecoration() {
-    this.findEditeContent();
-    // this.reviewType.forEach((el) => {
-    //   this.editor.setDecorations(el.type, el.rangs);
-    // });
-    this.editor.setDecorations(DecorationType, this.reviewType);
+    // 先对数组从小到大排序
+    if (this.reviewType.length > 0) return false;
+    this.reviewType.sort((a, b) => {
+      return a.decoration.range._start._line - b.decoration.range._start._line;
+    });
+
+    let tempArray = [];
+    // 以两两为一组
+    for (let index = 0; index < this.reviewType.length; index += 2) {
+      const element = this.reviewType[index];
+
+      const next = this.reviewType[index + 1];
+
+      const tempObj = {
+        type: element.type,
+        decoration: [element.decoration, next.decoration],
+      };
+
+      tempArray.push(tempObj);
+    }
+
+    tempArray.forEach((el) => {
+      const DecorationType = DecorationTypes[el.type];
+
+      this.editor.setDecorations(DecorationType, el.decoration);
+    });
+
+    console.log("rang范围", tempArray);
   }
 }
 module.exports = Decoration;
