dc4c74b9aa2b711fc2afc4e16cf278c6a90eadc3
王港 <wangg11@mingyuanyu.com>
2021-08-27 16:32:07 +0800

    增加清空console

--- module/clearCD.js
+++ module/clearCD.js
@@ -1,36 +1,26 @@
 /**
  * 清空console 和 debugger
  */
-const strUtils = require("../utils/string");
-const vscode = require("vscode");
-// @ts-ignore
-const TextEncoder = require("fast-text-encoding");
+const { writeFileSync } = require("fs");
 
 const clearCd = (editor) => {
   // 获取用户文本内容
-  console.log(editor.document, "文本内容");
+  console.log(editor, "文本内容");
   let Text = editor.document.getText();
   const reg = /(console\.log(.*);?)|(debugger\S*)/g;
-  let writeText = Text.replace(reg, "");
-  console.log(writeText, "清空的");
-  // editor.document.set;
-  vscode.workspace.fs.writeFile(vscode.Uri.file(editor.document.fileName), strUtils.stringToUint8Array(writeText));
+  const lines = Text.split("\n");
+  const handledLines = [];
+  // 逐行删除console和debugger
+  for (const line of lines) {
+    if (!reg.test(line)) {
+      handledLines.push(line);
+    }
+  }
+
+  let handledContent = handledLines.join("\n");
+  console.log(handledContent, "清空的", editor.document.fileName);
+  //  vscode.workspace.fs.writeFile(vscode.Uri.file(editor.document.fileName), strUtils.stringToUint8Array(handledContent));
+  if (Text != handledContent) writeFileSync(editor.document.fileName, handledContent, { encoding: "utf-8" });
 };
 
 module.exports = clearCd;
-
-// const text = `
-// export const GET_WEBSITE_CONFIG = 'app/';
-// export function getWebsiteConfig() {}
-
-// console.log('xxx');
-// debugger
-// `;
-// console.log(text.match(/^.*[console.log('\S')]/));
-
-// console.log(
-//   text.replace(/(console\.log(.*))|(debugger)/g, (str) => {
-//     console.log(str, "匹配到");
-//     return "";
-//   })
-// );
